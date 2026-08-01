dd4577219c4e27129bf68eb00e04c7f0b143176f
Aly Raffauf <aly@aly.codes>
2026-07-31 20:04:23 -0400

    fix: restore mobile viewport

--- src/root.tsx
+++ src/root.tsx
@@ -42,6 +42,7 @@ export default function Root() {
     <html lang="en" className="bg-pink-100 text-zinc-900">
       <head>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
